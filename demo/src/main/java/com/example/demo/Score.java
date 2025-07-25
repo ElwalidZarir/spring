33aba8e765e8ee8adccec27af85e29b2c1a91bbf
elwalidzarir <wazardo99@gmail.com>
2025-07-25 13:41:28 +0100

    refactor score code

--- demo/src/main/java/com/example/demo/Score.java
+++ demo/src/main/java/com/example/demo/Score.java
@@ -5,19 +5,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class Score {
-     AtomicInteger wins = new AtomicInteger(30);
-     AtomicInteger ties = new AtomicInteger(20);
-     AtomicInteger losses = new AtomicInteger(10);
+public record Score(AtomicInteger wins,  AtomicInteger ties,AtomicInteger losses) {
 
-    public Score() {
-    }
-
-    public Score(AtomicInteger wins, AtomicInteger ties, AtomicInteger losses) {
-        this.wins = wins;
-        this.ties = ties;
-        this.losses = losses;
-    }
 }
--- demo/src/main/java/com/example/demo/ScoreController.java
+++ demo/src/main/java/com/example/demo/ScoreController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @CrossOrigin
 public class ScoreController {
-    static Score score = new Score();
+    static Score score = new Score(new AtomicInteger(30),new AtomicInteger(20),new AtomicInteger(1));
 
     @GetMapping("/health-check")
     public String getHealthCheck() {
@@ -27,23 +27,23 @@ public class ScoreController {
 
     @PostMapping("/score/wins")
     public Score increasWin() {
-        score.getTies().incrementAndGet();
+        score.ties().incrementAndGet();
         return score;
     }
 
     @GetMapping("/score/wins")
     public AtomicInteger getwins() {
-        return score.wins;
+        return score.wins();
     }
 
     @GetMapping("/score/losses")
     public AtomicInteger getLosses() {
-        return score.losses;
+        return score.losses();
     }
 
     @GetMapping("/score/ties")
     public AtomicInteger getTies() {
-        return score.ties;
+        return score.ties();
     }
     
     
